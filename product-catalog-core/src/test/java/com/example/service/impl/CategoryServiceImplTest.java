fb6aef6480a7656736fdb143852373350d23cf9d
antbtv <antonbut48@gmail.com>
2025-05-06 21:45:20 +0300

    Add preparing for the tests

---
+++ product-catalog-core/src/test/java/com/example/service/impl/CategoryServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.example.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceImplTest {
+
+    @org.junit.jupiter.api.Test
+    void createCategory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCategoryById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateCategory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteCategory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllCategories() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void exportCategoriesToJson() {
+    }
+}
